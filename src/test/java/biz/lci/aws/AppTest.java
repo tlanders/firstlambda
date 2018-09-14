7283ed9fdfd4fa5cec58fa8f9b68f42c9174bf7d
tlanders <tlanders@landersconsulting.biz>
2018-09-14 15:17:18 -0500

    initial commit

---
+++ src/test/java/biz/lci/aws/AppTest.java
@@ -0,0 +1,20 @@
+package biz.lci.aws;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
