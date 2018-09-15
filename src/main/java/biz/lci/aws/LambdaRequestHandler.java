604b7cdf627707fe939a697e0a378d68feaf5470
tlanders <tlanders@landersconsulting.biz>
2018-09-15 16:45:37 -0500

    added comment to handler method

--- src/main/java/biz/lci/aws/LambdaRequestHandler.java
+++ src/main/java/biz/lci/aws/LambdaRequestHandler.java
@@ -4,6 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class LambdaRequestHandler implements RequestHandler<String, String> {
+    /**
+     * Sample request handler for AWS Lambda. This method must be passed a string (i.e. "XXX")
+     * and not JSON. JSON will fail.
+     * @param input
+     * @param context
+     * @return  A result string based on the input.
+     */
     public String handleRequest(String input, Context context) {
         context.getLogger().log("Input: " + input);
         return "Hello World - " + input;
